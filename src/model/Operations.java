343bc55474ac0ee4035286da75589b76dab4b85a
PaulaTrujillo27 <paulatrujillo536@gmail.com>
2020-10-18 17:41:31 -0500

    Second Version

--- src/model/Operations.java
+++ src/model/Operations.java
@@ -5,15 +5,22 @@ public class Operations{
 	public static final int FINALCONSTRUCTION=2600000;
 	public static final int PAINTING=980000;
 	
-	public static double TotalPayStores(int materials, double[] priceHCDefinitive, int[] amountMaterial){
+	
+	/** 
+     *Calculates the address depending on the user's address. <br>
+     * <b> pre: </b> The number entered can ONLY be 1, 2 or 3.
+     * @param totalHC Type Double, materials!=0.
+     * @return totalHC returns each the total prices of each of the materials entered.
+     */	
+	public static double TotalPayStores(int materials, double[] priceHCDefinitive, int[] amountMaterial, int locationDefinitive){
 		double totalHC=0; 
 		for(int i = 0; i < materials; i++){
-			totalHC = priceHCDefinitive[i] * amountMaterial[i];
+			totalHC+= priceHCDefinitive[i] * amountMaterial[i];
 		}
 		switch (locationDefinitive){
 			case 1:
 			if (totalHC<80000){
-				totalHC=+ 120000;				
+				totalHC+= 120000;				
 			}
 			else if(totalHC<300000){
 				totalHC+=28000;
@@ -22,13 +29,13 @@ public class Operations{
 			
 			case 2:
 				if (totalHC<300000){
-					totalHC=+50000;				
+					totalHC+=50000;				
 				}
 			break;
 			
 			case 3:
 				if (totalHC<80000){
-					totalHC=+ 120000;				
+					totalHC+= 120000;				
 				}
 				else if(totalHC<300000){
 					totalHC+=55000;
@@ -41,72 +48,24 @@ public class Operations{
 		return totalHC;
 	}
 	
-	
-	/*public static double TotalPayDowntown( int materials, double[] priceDowntown, int[] amountMaterial){
-		double countDowntown = 0; 
-		for(int i = 0; i < materials; i++){
-			
-			double totalDowntown = priceDowntown[i] * amountMaterial[i];
-			countDowntown = (totalDowntown + countDowntown);	
-		}
-		countDowntown=countDowntown+ROUGHCONSTRUCTION+FINALCONSTRUCTION+PAINTING;
-		return countDowntown;
-	}
-	
-	
-	public static double TotalPayNBH( int materials, double[] priceNBH, int[] amountMaterial){
-		double countNBH = 0; 
-		for(int i = 0; i < materials; i++){
-			
-			double totalNBH = priceNBH[i] * amountMaterial[i];
-			countNBH = totalNBH + countNBH;
-		}
-		countNBH=countNBH+ROUGHCONSTRUCTION+FINALCONSTRUCTION+PAINTING;
-		return countNBH;
-	}*/
-	
-	/*public static double totalStores (double[]priceHCDefinitive,int locationDefinitive, int materialsDefinitive){
-		double total_stores=0;
-		for (int i=0;i<materialsDefinitive;i++){
-			total_stores=total_stores+priceHC[i];
-		}
-			switch (locationDefinitive){
-				case 1:
-				if (total_stores<80000){
-					total_stores=+ 120000;				
-				} else if (total_stores<300000){
-					total_stores+=28000;
-				}
-				break;
-				case 2:
-					if (total_stores<300000){
-					total_stores=+50000;				
-				}
-				break;
-				case 3:
-				if (total_stores<80000){
-					total_stores=+ 120000;				
-				} else if (total_stores<300000){
-					total_stores+=55000;
-				}
-				break;
-				default:
-			}
-		total_stores=total_stores+ROUGHCONSTRUCTION+FINALCONSTRUCTION+PAINTING;
-		return total_stores;
-		
-	}*/
-	
+
+    /** 
+     *Calculates the address depending on the user's address. <br>
+     * <b> pre: </b> The number entered can ONLY be 1, 2 or 3.
+     * @param totalHC Type Double, materials!=0.
+     * @return totalHC returns each the total prices of each of the materials entered.
+     */	
 	public static double [] bestPrice(double[] priceHCDefinitive, double []priceNBHDefinitive, double [] priceDowntownDefinitive, int materialsDefinitive, int locationDefinitive){
-		double[] best_price_stores=0;
-		for(int=0;i<materialsDefinitive;i++){
+		double[] best_price_stores= new double [materialsDefinitive];
+		for(int i=0;i<materialsDefinitive;i++){
 			if( priceHCDefinitive[i]<priceDowntownDefinitive[i] &&  priceHCDefinitive[i]<priceNBHDefinitive[i]){
-				best_price_stores[i]=priceHCDefinitive[i];
-			}	
-			}else if(priceDowntownDefinitive[i]<priceHCDefinitive[i]) && priceDowntownDefinitive[i]<priceNBHDefinitive[i]){
+				best_price_stores[i]=priceHCDefinitive[i];	
+			}
+			else if(priceDowntownDefinitive[i]<priceHCDefinitive[i] && priceDowntownDefinitive[i]<priceNBHDefinitive[i]){
 				best_price_stores[i]=priceDowntownDefinitive[i];
 				
-			}else if (priceNBHDefinitive[i]< priceHCDefinitive[i]) && (priceNBHDefinitive[i]<priceDowntownDefinitive[i]){
+			}
+			else if(priceNBHDefinitive[i]< priceHCDefinitive[i] && priceNBHDefinitive[i]<priceDowntownDefinitive[i]){
 				best_price_stores[i]=priceNBHDefinitive[i];
 			}	
 			
@@ -115,16 +74,22 @@ public class Operations{
 	}
 	
 	
+	/** 
+     *Calculates the address depending on the user's address. <br>
+     * <b> pre: </b> The number entered can ONLY be 1, 2 or 3.
+     * @param totalHC Type Double, materials!=0.
+     * @return totalHC returns each the total prices of each of the materials entered.
+     */	
 	public static String[] bestStore(double[] priceHCDefinitive, double []priceNBHDefinitive, double [] priceDowntownDefinitive, int materialsDefinitive, int locationDefinitive){
-		String[] bestStores=0;
-		for(int=0;i<materialsDefinitive;i++){
+		String[] bestStores=new String[materialsDefinitive];
+		for(int i=0;i<materialsDefinitive;i++){
 			if( priceHCDefinitive[i]<priceDowntownDefinitive[i] &&  priceHCDefinitive[i]<priceNBHDefinitive[i]){
-				bestStores[i]="HomeCenter";
-			}	
-			}else if(priceDowntownDefinitive[i]<priceHCDefinitive[i]) && priceDowntownDefinitive[i]<priceNBHDefinitive[i]){
+				bestStores[i]="HomeCenter";	
+			}
+			else if(priceDowntownDefinitive[i]<priceHCDefinitive[i] && priceDowntownDefinitive[i]<priceNBHDefinitive[i]){
 				bestStores[i]="DowntownStore";
 				
-			}else if (priceNBHDefinitive[i]< priceHCDefinitive[i]) && (priceNBHDefinitive[i]<priceDowntownDefinitive[i]){
+			}else if(priceNBHDefinitive[i]< priceHCDefinitive[i] && priceNBHDefinitive[i]<priceDowntownDefinitive[i]){
 				bestStores[i]="Neighborhood";
 			}	
 			
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -11,25 +11,29 @@ public class Main{
 		
 		int materialsDefinitive=amountTotalMaterials();
 		String [] namesDefinitive=getMaterials(materialsDefinitive);
-		double [] amountEachMaterialDefinitive= amountEachMaterial(namesDefinitive,materialsDefinitive);
+		int [] amountEachMaterialDefinitive= amountEachMaterial(namesDefinitive,materialsDefinitive);
 		double[] priceHCDefinitive=HomeCenterPrice (namesDefinitive,materialsDefinitive);
 		double[] priceDowntownDefinitive=DowntownStore(namesDefinitive, materialsDefinitive);
 		double[] priceNBHDefinitive=neighborhoodStore (namesDefinitive, materialsDefinitive);
 		int[] workTypeDefinitive=materialsUse(namesDefinitive,materialsDefinitive);
 		int locationDefinitive=houseLocation();
-		System.out.println("The total to pay of the work in HomeCenter is : " +Operations.TotalPayStores(  materials,priceHCDefinitive,amountEachMaterialDefinitive));
-		System.out.println("The total to pay of the work in Downtown Store is : " +Operations.TotalPayStores(materials,priceDowntownDefinitive,amountEachMaterialDefinitive));
-		System.out.println("The total to pay of the work in   is : " +Operations.TotalPayStores(materials,priceNBHDefinitive,amountEachMaterialDefinitive);
-		double[] bestPriceDefinitive=+Operations.bestPrice(priceHCDefinitive,priceNBHDefinitive, priceDowntownDefinitive,materialsDefinitive, locationDefinitive);
-		String[] bestStoreDefinitive=+Operations. bestStore(priceHCDefinitive,priceNBHDefinitive,priceDowntownDefinitive,materialsDefinitive,locationDefinitive);
-		for(int i=0, i<materialsDefinitive,i++){
+		System.out.println("The total to pay of the work in HomeCenter is : " +Operations.TotalPayStores( materialsDefinitive,priceHCDefinitive,amountEachMaterialDefinitive,locationDefinitive));
+		System.out.println("The total to pay of the work in Downtown Store is : " +Operations.TotalPayStores(materialsDefinitive,priceDowntownDefinitive,amountEachMaterialDefinitive,locationDefinitive));
+		System.out.println("The total to pay of the work in   is : " +Operations.TotalPayStores(materialsDefinitive,priceNBHDefinitive,amountEachMaterialDefinitive,locationDefinitive));
+		double[] bestPriceDefinitive=Operations.bestPrice(priceHCDefinitive,priceNBHDefinitive, priceDowntownDefinitive,materialsDefinitive, locationDefinitive);
+		String[] bestStoreDefinitive=Operations.bestStore(priceHCDefinitive,priceNBHDefinitive,priceDowntownDefinitive,materialsDefinitive,locationDefinitive);
+		for(int i=0;i<materialsDefinitive;i++){
 		System.out.println("The best price of the material " +namesDefinitive[i]+ "Is: " +bestPriceDefinitive[i]+ "in the: " +bestStoreDefinitive[i]+ " Store ");
 		}
-		
 		useTypeSelection(workTypeDefinitive,namesDefinitive,materialsDefinitive);
 	}
 	
-
+	/** 
+     *The user enters the amount of materials to be purchased. <br>
+     * <b> pre: </b> the number entered must be integer and positive.
+     * @param materials Type Int.
+     * @return materials returns the amount of materials to be entered later.
+     */
 	public static int  amountTotalMaterials(){
 	System.out.println("Enter the quantity of materials to buy");
 	int materials = reader.nextInt();
@@ -37,6 +41,12 @@ public class Main{
 	return materials;
     }
 	
+	/** 
+     *The user enters the the name of each of the materials to buy.<br>
+     * <b> pre: </b> the number entered must be integer and positive.
+     * @param materials Type Int.
+     * @return materials returns the amount of materials to be entered later.
+     */
 	public static String[] getMaterials(int materialsDefinitive){
 		String[] names = new String[materialsDefinitive];
 		for(int i=0; i<materialsDefinitive;i++){
@@ -46,8 +56,15 @@ public class Main{
 		return names;
 	}		
 
-	public static double [] amountEachMaterial(String[] namesDefinitive, int materialsDefinitive){
-		double [] amountMaterial = new double [materialsDefinitive];
+
+    /** 
+     *The user enters the quantity of each of the materials to buy. <br>
+     * <b> pre: </b> the number entered must be integer and positive.
+     * @param amounMaterial Array Int.
+     * @return amountMaterial returns the quantity of each of the materials to buy.
+     */
+	public static int [] amountEachMaterial(String[] namesDefinitive, int materialsDefinitive){
+		int [] amountMaterial = new int [materialsDefinitive];
 		for(int i=0; i<materialsDefinitive;i++){
 			System.out.println("Enter the quantity of the material "+namesDefinitive[i]);
 			amountMaterial[i]=reader.nextInt();
@@ -56,28 +73,47 @@ public class Main{
 		return amountMaterial;
 	}	
 	
+	
+	/** 
+     *The user enters the cost of each of the materials in the HomeCenter store. <br>
+     * <b> pre: </b> The amount entered can be a whole number and decimal but it has to be POSITIVE.
+     * @param priceHC Array Double.
+     * @return priceHC returns the price in the HomeCenter Store of each of the materials previously entered.
+     */
 	public static double [] HomeCenterPrice (String [] namesDefinitive, int materialsDefinitive){
 		double[] priceHC = new double [materialsDefinitive]; 
 		for(int i=0; i<materialsDefinitive;i++){
 			System.out.println("Enter the cost of the material "+namesDefinitive[i]+" in HomeCenter");
 			priceHC[i]=reader.nextDouble();
-			reader.nextDouble();
+			reader.nextLine();
 		}
 		return priceHC;
 	}
 	
-
+    
+	/** 
+     *The user enters the cost of each of the materials in the Downtown store. <br>
+     * <b> pre: </b> The amount entered can be a whole number and decimal but it has to be POSITIVE.
+     * @param priceDowntown Array Double.
+     * @return priceDowntown returns the price in the Downtown Store of each of the materials previously entered.
+     */
 	public static double [] DowntownStore(String [] namesDefinitive, int materialsDefinitive){
 		double[] priceDowntown = new double [materialsDefinitive]; 
 		for(int i=0; i<materialsDefinitive;i++){
 			System.out.println("Enter the cost of the material "+namesDefinitive[i]+" in the Downtown's Store");
 			priceDowntown[i]=reader.nextDouble();
-			reader.nextDouble();
+			reader.nextLine();
 		}
 		return priceDowntown;
 	}
 	
 	
+	/** 
+     *The user enters the cost of each of the materials in the Neighborhood store. <br>
+     * <b> pre: </b> The amount entered can be a whole number and decimal but it has to be POSITIVE.
+     * @param priceNBH Array Double.
+     * @return priceNBH returns the price in the Neighborhood Store of each of the materials previously entered.
+     */
 	public static double [] neighborhoodStore (String [] namesDefinitive, int materialsDefinitive){
 		double[] priceNBH = new double [materialsDefinitive]; 
 		for(int i=0; i<materialsDefinitive;i++){
@@ -89,7 +125,12 @@ public class Main{
 	}
 
 	
-		
+	/** 
+     *The user enters the type of use of each of the materials. With the option of: if you enter 1 the utility will be ROUGHCONSTRUCTION, 2 the utility will be FINALCONSTRUCTION and 3 the utility will be PAINTING <br>
+     * <b> pre: </b> The number entered can ONLY be 1, 2 or 3.
+     * @param workType Array Int.
+     * @return workType returns if the type of use of each of the materials will be ROUGHCONSTRUCTION, FINALCONSTRUCTION O PAINTING.
+     */	
 	public static int [] materialsUse(String []namesDefinitive, int materialsDefinitive){
 		int[] workType = new int [materialsDefinitive]; 
 		for(int i=0; i<materialsDefinitive;i++){
@@ -101,7 +142,12 @@ public class Main{
 	}
 	
 
-	
+	 /** 
+     *The user enters a number (I can ONLY be 1, 2 or 3) where each one displays the names of the materials by type of use. Being 1 ROUGHCONSTRUCTION, 2 FINALCONSTRUCTION and 3 PAINTING <br>
+     * <b> pre: </b> The number entered can ONLY be 1, 2 or 3.
+     * @param utility Type Int.
+     * @return utility returns the materials according to the type of use chosen above.
+     */	
 	public static void useTypeSelection (int [] workTypeDefinitive, String[] namesDefinitive, int materialsDefinitive){
 		System.out.println("what use type do you want to see your materials for?");
 		System.out.println("Enter 1 for ROUGHCONSTRUCTION, 2 for FINALCONSTRUCTION, 3 for PAINTING");
@@ -130,7 +176,12 @@ public class Main{
 	}
 	
 	
-	
+	/** 
+     *The user enters where is your address located. Being 1 North, 2 Central and 3 South. <br>
+     * <b> pre: </b> The number entered can ONLY be 1, 2 or 3.
+     * @param location Type Int.
+     * @return location returns if the address is in the north, center or south.
+     */	
 	public static int houseLocation(){
 		System.out.println("Where is your address located?");
 		System.out.println("Enter 1 if it is North, 2 if it is Central and 3 if it is South");
